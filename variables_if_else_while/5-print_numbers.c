bf9014b8fececb28d364c71aa896b9532522483d
root <7448@holbertonstudents.com>
2023-10-16 06:00:24 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - alphabet with exeption
+ * @void: void
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
