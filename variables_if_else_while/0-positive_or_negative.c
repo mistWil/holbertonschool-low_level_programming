4b4dbb95298f79d56e6f3b776bff482db8427b5c
root <7448@holbertonstudents.com>
2023-10-16 01:59:01 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <math.h>
 /**
- *  Task 0: Positive or negative
- *  Edit by : Wil L.i
+ *  main: Positive or negative
+ *  return 0 (for success)
  *
  */  
  
@@ -18,20 +18,17 @@ int main(void)
       	n = rand() - RAND_MAX / 2;
 
       	/* Conditions*/
-
 	if (n < 0)
 	{
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	} 
-	
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	} 
-	
 	else 
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}	
 
 	return (0); 
