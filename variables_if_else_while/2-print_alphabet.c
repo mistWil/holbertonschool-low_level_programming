d4ebd52e1f76ef335ad4f43e105168b771a38fc5
root <7448@holbertonstudents.com>
2023-10-16 04:52:18 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - void
+ * @void: void
+ *
+ * Return : 0 if successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	return (0);
+}
