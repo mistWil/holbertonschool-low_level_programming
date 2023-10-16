34c9256688666dea198a64aba891ef9c72d891cc
root <7448@holbertonstudents.com>
2023-10-16 01:03:07 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,13 +12,13 @@ int main(void)
 
 	if (n < 0)
 	{
-		printf("is positive");
+		printf("is positive\n");
 	} else if (n=0)
 	{
-		printf("is zero");
+		printf("is zero\n");
 	} else 
 	{
-		printf("is negative");
+		printf("is negative\n");
 	}	
 
 	return (0); 
