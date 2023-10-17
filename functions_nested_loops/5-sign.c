75c6b0c8a50ebf25a883106350806143bd2c57b9
root <7448@holbertonstudents.com>
2023-10-17 07:06:05 -0700

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,19 +4,24 @@
  * print_sign - positive or negative
  * @n: char to check
  *
- * Return: 1 if positive 0 if not
+ * Return: 1 if positive 0 if egal 0 and -1 if negative
  */
 
 int print_sign(int n)
 {
-	if (n >= '0')
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 	else
 	{
 		_putchar('-');
-		return (0);
+		return (-1);
 	}
 }
