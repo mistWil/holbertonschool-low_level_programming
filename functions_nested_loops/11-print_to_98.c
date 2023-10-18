69b3e98065a74284dbe54438d16406792f9ef865
root <7448@holbertonstudents.com>
2023-10-18 03:32:36 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,31 +9,13 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n != 98)
 	{
-		while (n < 98)
-		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
-			n++;
-		}
-		_putchar('\n');
-	}
-	else if (n == 98)
-	{
-		_putchar(n);
-		_putchar('\n');
-	} 
-	else
-	{
-		while(n > 98)
-		{
-			_putchar(n);
-			_putchar(',');
-                        _putchar(' ');
+		printf("%i, ", n);
+		if (n > 98)
 			n--;
-		}
-		_putchar('\n');
+		else
+			n++;
 	}
+	printf("98\n");
 }
