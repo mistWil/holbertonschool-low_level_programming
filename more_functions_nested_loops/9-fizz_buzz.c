bcd774bb39def01a4bc7785c1b1238ffeb31b4fc
root <7448@holbertonstudents.com>
2023-10-20 05:29:49 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -17,21 +17,25 @@ int main(void)
 		if ((i % 3 == 0) && (i % 5 != 0))
 		{
 			printf("Fizz");
+			if (i != 100)
 			printf(" ");
 		}
 		else if ((i % 5 == 0) && (i % 3 != 0))
 		{
 			printf("Buzz");
+			if (i != 100)
 			printf(" ");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
+			if (i != 100)
 			printf(" ");
 		}
 		else
 		{
 			printf("%d", i);
+			if (i != 100)
 			printf(" ");
 		}
 	}
