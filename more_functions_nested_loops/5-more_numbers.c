a784c50cd46e5a5b615de6ab4482d7d0a9a1a203
root <7448@holbertonstudents.com>
2023-10-20 02:33:39 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - from 0 to 9 followed by a new line
+ * @void: void
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
+void more_numbers(void);
