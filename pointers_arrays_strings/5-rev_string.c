32176080f122a117bb6892fdbbcfe227b8fb13ab
mistWil <7448@holbertonstudents.com>
2023-10-24 01:45:09 -0700

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - print string reverse
+ * @s: char
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	while (len >= 0)
+	{
+		s--;
+		len--;
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
+
