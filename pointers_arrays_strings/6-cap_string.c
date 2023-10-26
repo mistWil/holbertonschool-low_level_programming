417c135d9ddb1f5f5daaf83e5d9bddc0e3f6f25d
mistWil <7448@holbertonstudents.com>
2023-10-26 00:59:12 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,11 +12,26 @@ char *cap_string(char *a)
 	char *convert = a;
 	int len = 0;
 
-	while (a[len] !='\0')
+	if (a[0] >= 'a' && a[0] <= 'z')
 	{
-		if (a[len] == ' ' || a[len] == '.' || a[len] == '\t' || a[len] == '\n' || a[len] == ','
-				|| a[len] == ';' || a[len] == '!' || a[len] == '?' || a[len] == '"' || a[len] == '('
-				|| a[len] == ')' || a[len] == '{' || a[len] == '}')
+		a[len] = a[len] - 32;
+	}
+
+	while (a[len] != '\0')
+	{
+		if (a[len] == ' ' ||
+		a[len] == '.' ||
+		a[len] == '\t' ||
+		a[len] == '\n' ||
+		a[len] == ',' ||
+		a[len] == ';' ||
+		a[len] == '!' ||
+		a[len] == '?' ||
+		a[len] == '"' ||
+		a[len] == '(' ||
+		a[len] == ')' ||
+		a[len] == '{' ||
+		a[len] == '}')
 		{
 			if (a[len + 1] >= 'a' && a[len + 1] <= 'z')
 			{
