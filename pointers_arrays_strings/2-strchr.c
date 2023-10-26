37cd3bd1955aeed7c34a0cd85af9629c8cb6bdd5
mistWil <7448@holbertonstudents.com>
2023-10-26 05:24:59 -0700

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locate caracter in string
+ * @s: char
+ * @c: char
+ *
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,4 +23,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
