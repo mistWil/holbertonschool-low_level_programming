b1301ed909b7bc04d59e1a46caceecb21c36e6db
mistWil <7448@holbertonstudents.com>
2023-11-08 01:29:46 -0800

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0)
 		return (NULL);
 	if (size == 0)
-		return NULL;
+		return (NULL);
 
 	array = malloc(sizeof(unsigned int) * nmemb);
 
