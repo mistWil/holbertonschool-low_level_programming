dbc3accc12035367bc3c65338b1f2a9bb0481a4c
mistWil <7448@holbertonstudents.com>
2023-11-13 01:47:52 -0800

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - structure declaration
+ * @name: char
+ * @age: int
+ * @owner: char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
