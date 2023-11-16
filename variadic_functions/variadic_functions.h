d4796096c503a7338a1e54a5dc1e3734be98e9c1
mistWil <7448@holbertonstudents.com>
2023-11-16 07:53:09 -0800

    3-print_all.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -11,4 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/*Structure*/
+typedef struct form
+{
+	char c;
+	int i;
+	float f;
+	char *s;
+}format;
+
 #endif
