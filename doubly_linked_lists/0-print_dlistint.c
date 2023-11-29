a8b4e8281331558a7bd29cf1a1022e2dd14cfc0e
mistWil <7448@holbertonstudents.com>
2023-11-29 02:39:19 -0800

    0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -5,13 +5,14 @@
  * elements of a dlistin_t list.
  * @h: pointeur to the head of dlistin_t
  *
- * Return: size_t
+ * Return: number of node
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	unsigned int cpt = 0;
-	const dlistint_t *current = h;
+	const dlistint_t *current;
+	current = h;
 
 	while (current != NULL)
 	{
