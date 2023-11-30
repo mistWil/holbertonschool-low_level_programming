2cbea79b86229d1d27097a0184d3907f641a35c3
mistWil <7448@holbertonstudents.com>
2023-11-30 06:24:59 -0800

    change while condition

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -14,7 +14,7 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *current;
 	current = h;
 
-	while (current != NULL)
+	while (current)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
