f9b661b458c5ec8b91870ae7dfa92c6155241fa1
mistWil <7448@holbertonstudents.com>
2023-11-30 04:55:16 -0800

    all files of task8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,7 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current, *temp;
+	dlistint_t *current;
 	unsigned int i;
 
 	if (head == NULL || *head == NULL)
