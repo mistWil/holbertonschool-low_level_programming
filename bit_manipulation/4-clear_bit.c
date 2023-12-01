4bc31a7fe9a23d1c600e3986f0b17b95ae9549fb
mistWil <7448@holbertonstudents.com>
2023-12-01 02:20:12 -0800

    4-clear_bit.c

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,9 +9,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8)
-        return -1;
+	if (index >= sizeof(unsigned long int) * 8)
+	    return -1;
 
-    *n = *n & ~(1UL << index);
-    return (1);
+	*n = *n & ~(1UL << index);
+	return (1);
 }
