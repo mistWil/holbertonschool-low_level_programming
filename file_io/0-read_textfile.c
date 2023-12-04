675e7de69c3d1009fdbed4abe8faefb0127ffd6c
mistWil <7448@holbertonstudents.com>
2023-12-04 01:57:03 -0800

    updating betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints
+ * it to the POSIX standard output.
  * @filename: Name of the file to read.
  * @letters: Number of letters to read and print.
- * Return: Actual number of letters read and printed; 0 if an error occurs.
+ * Return: Actual number of letters read and printed;
+ * 0 if an error occurs.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;     /* File descriptor*/
@@ -15,12 +16,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	/*Open the file for reading*/
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	/*Allocate memory for the buffer*/
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
@@ -28,7 +27,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	/*Read from the file*/
 	read_bytes = read(fd, buffer, letters);
 	if (read_bytes == -1)
@@ -37,7 +35,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	/*Write to the standard output*/
 	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
 	if (write_bytes == -1 || write_bytes != read_bytes)
@@ -46,7 +43,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	/*Clean up and close the file*/
 	free(buffer);
 	close(fd);
