38ca244017ddc7a646f0a367880cc83d026d12c4
mistWil <7448@holbertonstudents.com>
2023-12-07 05:06:58 -0800

    1-djb2.c

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -36,5 +36,7 @@ typedef struct hash_table_s
 
 /*PROTOTYPES DE FONCTIONS*/
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+
 
 #endif
