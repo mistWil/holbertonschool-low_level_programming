4022ef1ca5f1d6c32fe289b1c942ca3dfa1940af
mistWil <7448@holbertonstudents.com>
2023-12-07 04:49:02 -0800

    0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of the array
+ *
+ * Return: A pointer to the newly created hash table, or NULL if an error occurs
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL;
+
+	if (size < 1)
+		return (NULL);
+
+	/* Allouer de la mémoire pour la table de hachage */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	/* Allouer de la mémoire pour le tableau de pointeurs */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	/* Initialiser chaque pointeur à NULL */
+	for (unsigned long int i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
+	new_table->size = size;
+
+	return (new_table);
+}
